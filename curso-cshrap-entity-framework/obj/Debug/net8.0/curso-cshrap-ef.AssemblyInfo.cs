6d59d4017ba7dd8b6e1c19563a00c32b6214ae9d
dlscoccia <dlscoccia@gmail.com>
2024-09-12 20:03:33 -0400

    API Template - .NET

--- curso-cshrap-entity-framework/obj/Debug/net8.0/curso-cshrap-ef.AssemblyInfo.cs
+++ curso-cshrap-entity-framework/obj/Debug/net8.0/curso-cshrap-ef.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curso-cshrap-ef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46bcbf9028e5c04fbac622e975e615b21ac2743")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18d4d06dd3c78fef7bab8cb67fc5cf18215ad61")]
 [assembly: System.Reflection.AssemblyProductAttribute("curso-cshrap-ef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curso-cshrap-ef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
